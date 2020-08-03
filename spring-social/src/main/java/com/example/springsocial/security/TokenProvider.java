0c281fc8372fbd1a7acd1d45309c376a7fdb726e
artium <artium59@gmail.com>
2020-08-03 17:18:09 +0900

    Update TokenProvider.java

--- spring-social/src/main/java/com/example/springsocial/security/TokenProvider.java
+++ spring-social/src/main/java/com/example/springsocial/security/TokenProvider.java
@@ -9,6 +9,13 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
+/*
+  header와 payload는 base64로 인코딩만 되므로 누구나 디코딩하여 확인할 수 있다.
+  따라서 payload에는 중요한 정보가 포함되면 안된다. 하지만 verify signature는 SECRET KEY를 알지 못하면 복호화할 수 없다.
+
+  https://brownbears.tistory.com/440
+*/
+
 @Service
 public class TokenProvider {
 
@@ -27,7 +34,7 @@ public class TokenProvider {
         Date expiryDate = new Date(now.getTime() + appProperties.getAuth().getTokenExpirationMsec());
 
         return Jwts.builder()
-                // sub
+                // sub: 사용자 고유 번호
                 .setSubject(Long.toString(userPrincipal.getId()))
                 // iat: JWT 발급 시간
                 .setIssuedAt(new Date())
@@ -41,15 +48,21 @@ public class TokenProvider {
     }
 
     public String getUidFromJwtToken(String token) {
+        // parseClaimsJws(token): token을 Jws로 파싱
+        // getBody(): token에 저장했던 data들이 담긴 claims return (PAYLOAD) (getHead(), getBody(), getSignature())
+        // getSubject(): return PAYLOAD의 sub
         return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
     }
 
     public String getEmailFromJwtToken(String token) {
+        // get("email", String.class): return PAYLOAD의 email
         return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().get("email", String.class);
     }
 
     public boolean validateToken(String authToken) {
         try {
+            // application.yml의 tokenSecret으로 복호화
+            // 성공하면 true, 아님 에러 or false
             Jwts.parser().setSigningKey(appProperties.getAuth().getTokenSecret()).parseClaimsJws(authToken);
             return true;
         } catch (SignatureException ex) {
