14063a907962f1da6f131bb50f29798c75e654bf
artium <artium59@gmail.com>
2020-08-09 01:03:04 +0900

    Update RestAuthenticationEntryPoint

--- spring-social/src/main/java/com/example/springsocial/security/RestAuthenticationEntryPoint.java
+++ spring-social/src/main/java/com/example/springsocial/security/RestAuthenticationEntryPoint.java
@@ -10,10 +10,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+// 사용자가 보안 HTTP 리소스를 요청하지만 인증되지 않은 경우 AuthenticationEntryPoint가 호출
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     private static final Logger logger = LoggerFactory.getLogger(RestAuthenticationEntryPoint.class);
 
+    /*
+      - 인증 체계의 시작
+
+      HttpServletRequest - that resulted in an AuthenticationException
+      HttpServletResponse - so that the user agent can begin authentication
+      AuthenticationException - that caused the invocation
+    */
     @Override
     public void commence(HttpServletRequest httpServletRequest,
                          HttpServletResponse httpServletResponse,
