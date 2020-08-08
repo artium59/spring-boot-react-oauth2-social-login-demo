acfd67097a4d4ace5b9413a2f8c622ae2e705563
artium <artium59@gmail.com>
2020-08-09 00:42:15 +0900

    Update TokenAuthenticationFilter

--- spring-social/src/main/java/com/example/springsocial/security/TokenAuthenticationFilter.java
+++ spring-social/src/main/java/com/example/springsocial/security/TokenAuthenticationFilter.java
@@ -16,6 +16,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/*
+  OncePerRequestFilter
+    - 모든 servlet 컨테이너에서 요청 발송당 단일 실행을 보장하는 것을 목적으로 하는 filter 기반 class
+    - authentication  및 access control 기능이 있는 주요 application servlet 앞에 배치되는 filter로 구현
+    - request가 servlet에 도착하기 전에 filter chain을 통과해야 하는데, 일부 filter는 한 번만!!! 수행해야 되서 이 filter가 필요하다.
+*/
+
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
@@ -27,6 +34,7 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     private static final Logger logger = LoggerFactory.getLogger(TokenAuthenticationFilter.class);
 
     @Override
+    // single request thread내에서 request당 1번씩 호출
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
             String jwt = getJwtFromRequest(request);
@@ -35,6 +43,9 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
                 Long userId = tokenProvider.getUserIdFromToken(jwt);
 
                 UserDetails userDetails = customUserDetailsService.loadUserById(userId);
+
+                // 사용자 이름과 암호를 간단하게 표시하도록 설계된 인증 구현
+                // UsernamePasswordAuthenticationToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities)
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
@@ -44,6 +55,8 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
             logger.error("Could not set user authentication in security context", ex);
         }
 
+        // 모든 filter는 chain방식으로 연결
+        // filter는 FilterChain을 사용하여 chain의 다음 filter를 호출하거나, 호출 filter가 chain의 마지막 filter인 경우 chain의 끝에 있는 resource를 호출
         filterChain.doFilter(request, response);
     }
 
